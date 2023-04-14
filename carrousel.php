af7254ff47aa0c609fc67fb2a6491d0aa6c4fc12
phi <philipephi123@gmail.com>
2023-04-14 09:21:53 -0400

    s11c2 Les boutons du carrousel

--- carrousel.php
+++ carrousel.php
@@ -1,10 +1,49 @@
 <?php
 /**
- * 
  * Plugin name: Carrousel Phi
  * Auteur: Le Vo Hoang Phi
- * Author URI:
+ * Author URI: https://github.com/vohoangphile
  * Description: Le carrousel permettra d'afficher dans une boîte modale animée les images d'une galerie
  * Version 1.0.0 
  * */ 
-?>
+
+
+ function mon_enqueue_css_js(){
+    $version_css = filemtime(plugin_dir_path( __FILE__ ) . "style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");     
+
+    wp_enqueue_style( 'em_plugin_carrousel_css',
+                     plugin_dir_url(__FILE__) . "style.css",
+                     array(),
+                     $version_css);
+
+    wp_enqueue_script(  'em_plugin_carrousel_js',
+                    plugin_dir_url(__FILE__) ."js/carrousel.js",
+                    array(),
+                    $version_js,
+                    true);
+
+}
+
+add_action('wp_enqueue_scripts', 'mon_enqueue_css_js');
+
+
+function creation_carrousel()
+{
+    return "<button id='carrousel__ouvrir'>Ouvrir le carrousel</button>
+    <div class='carrousel'>
+    <button class='carrousel__x'>X</button>
+    <figure class='carrousel__figure'></figure>
+    <form class='carrousel__form'></form>
+    </div> <!fin du carrousel -->
+    ";
+}
+add_shortcode('carrousel', 'creation_carrousel');
+
+
+
+
+
+
+
+
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,3 +1,4 @@
 (function() {
     console.log("Début du carrousel ");
 })()
+
