6d7598bbb58e5f43035d69a4254d1a9fa19742f2
phi <philipephi123@gmail.com>
2023-04-28 10:50:37 -0400

    s13c1 en cliqaunt sur une image de la galerie on change l'image du carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -13,75 +13,79 @@
     let fleche__2 = document.querySelector('.fleche__2');
 
     carrousel__ouvrir.addEventListener('mousedown', function(){
-        carrousel.classList.add('carrousel--activer') // erreur avec un scale aussi dans le css .carrousel scale: 1
-        ajouter_les_images_de_galerie()
-    })
-    carrousel__x.addEventListener('mousedown', function(){
+        index=0
+  
+        carrousel.classList.add('carrousel--activer')
+        affiche_image_carrousel()
+     })
+  
+     carrousel__x.addEventListener('mousedown', function(){
         carrousel.classList.remove('carrousel--activer')
-    })
-    /**
-     * Pour chaque image de la galerie l'ajouter dans le carrousel
-     */
-    function ajouter_les_images_de_galerie()
-    {
-        for(const elem of galerie__img ) {
-            ajouter_une_image_dans_carrousel(elem)
-            ajouter_un_radio_bouton_dans_le_carrousel()
-        }
-    }
-    /**
-     * Création dynamique d'une image pour le carrousel
-     * @param {*} elem une image de la galerie
-     */
-    let position = 0; // crée l'indextation, numéroter chacun des composants du carrousel
-    let index = 0; // l'image courante, POSITION ACTUELLE de l'image
-    let ancienIndex = -1; // -1 ,car il n'y a pas d'image choisie pour l'instant
-    function ajouter_une_image_dans_carrousel(elem) {
-        let img = document.createElement('img');
-        img.src = elem.getAttribute('src');
-        img.classList.add('carrousel__img');
-        console.log(img.src);
-        carrousel__figure.appendChild(img);
-    }
-
-    function ajouter_un_radio_bouton_dans_le_carrousel()
-    {
-        let rad = document.createElement('input')
-        rad.setAttribute('type','radio')
-        rad.setAttribute('name','carrousel__rad')
-        rad.classList.add('carrousel__rad')
-        rad.dataset.index = position
-        rad.addEventListener('mousedown', function(){
-            index =  this.dataset.index 
-            affiche_image_carrousel()         
+     })
+  /**
+   * Pour chaque image de la galerie l'ajouter dans le carrousel
+   */
+  let position = 0
+  let index = 0
+  let ancienIndex = -1
+  /* -- boucle qui permet construire le carrousel */
+     for (const elem of galerie__img){
+        elem.dataset.index = position
+        /* en cliquant sur une image de la galerie */
+        elem.addEventListener('mousedown', function(e){
+           /*
+           avant d'ouvrir la boîte modale il faut vérifier si elle n'est pas déjà ouverte
+           https://www.javascripttutorial.net/dom/css/check-if-an-element-contains-a-class/
+           la fonction contains() vous permettra de faire cette vérification
+           */
+           index = e.target.dataset.index
+           affiche_image_carrousel()
         })
-        position = position + 1 // incrémentation de la position
-        carrousel__form.append(rad)
-    }   
-    /**
-     * Affiche la nouvelle image du carrousel
-     */
-    function affiche_image_carrousel() {
-        if (ancienIndex != -1){
-            carrousel__figure.children[ancienIndex].style.opacity = "0"
-            //carrousel__figure.children[ancienIndex].classList.remove('carrousel__img--activer')
-            }
-            //console.log(this.dataset.index)
-            carrousel__figure.children[index].style.opacity = "1" // rendre visible la nouvelle image
-            // carrousel__figure.children[index].classList.add('carrousel__img--activer')
-            ancienIndex = index
-    }
-    // function changer_image_fleche() {
-        
-    // }
-    console.log("Voici", fleche__1);
-    fleche__1.addEventListener('click', function(){
-        console.log("ALAIDE");
-        index = index + -1 // incrémentation de la position
-    })
-    fleche__2.addEventListener('click', function(){
-        console.log("AYUDA");
-        index = index + 1 // incrémentation de la position
-    })
+        ajouter_une_image_dans_courrousel(elem)
+        ajouter_un_radio_bouton_dans_carrousel()
+     }
+  
+  
+  /**
+   * Création dynamique d'une image pour le carrousel
+   * @param {*} elem une image de la galerie
+   */
+  function ajouter_une_image_dans_courrousel(elem)
+  {
+     let img = document.createElement('img')
+     img.classList.add('carrousel__img')
+     img.src = elem.src
+     // console.log(img.src)
+     carrousel__figure.appendChild(img);
+  }
+  
+  function ajouter_un_radio_bouton_dans_carrousel()
+  {
+     let rad = document.createElement('input')
+     rad.setAttribute('type','radio')
+     rad.setAttribute('name','carrousel__rad')
+     rad.classList.add('carrousel__rad')
+     rad.dataset.index = position
+     rad.addEventListener('mousedown', function(){
+       index =  this.dataset.index
+       affiche_image_carrousel()
+     })
+     position = position + 1 // incrémentation de la position
+     carrousel__form.append(rad)
+  }  
+  /**
+   * Affiche la nouvelle image du carrousel
+   */
+  function affiche_image_carrousel(){
+     if (ancienIndex != -1){
+        carrousel__figure.children[ancienIndex].style.opacity = "0"
+      //carrousel__form.children[ancienIndex].checked = false
+        //carrousel__figure.children[ancienIndex].classList.remove('carrousel__img--activer')
+       }
+       //console.log(this.dataset.index)
+       carrousel__figure.children[index].style.opacity = "1"
+      // carrousel__figure.children[index].classList.add('carrousel__img--activer')
+       ancienIndex = index
+  }
 })()
 
